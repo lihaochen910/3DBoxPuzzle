387495e3823691eb1c5224c4f3d8afd205608238
Kanbaru <lihaochen910@hotmail.com>
2017-08-15 00:03:51 +0800

    添加 代碼執行計時器

---
+++ Assets/Scripts/ExecuteTimer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public sealed class ExecuteTimer{
+
+    private static DateTime startTime;
+
+    public static void OnExecutePrepare()
+    {
+        startTime = System.DateTime.Now;
+    }
+
+    public static void OnExecuteEnd()
+    {
+        DateTime endTime = System.DateTime.Now;
+        TimeSpan ts = endTime.Subtract(startTime);
+        Debug.LogWarning("代码执行时间:" + ts.TotalMilliseconds.ToString("0.00ms"));
+    }
+}
